717bfc787bebcd28404322a289799019ab83f873
your_name_here <cssprasanth99@gmail.com>
2024-08-01 14:24:21 +0530

    added backend link

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -15,12 +15,12 @@ const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState(getDefaultCart());
 
   useEffect(() => {
-    fetch(`http://localhost:4000/allproducts`)
+    fetch(`https://wish-backend.onrender.com/allproducts`)
       .then((response) => response.json())
       .then((data) => setAll_Product(data));
 
     if (localStorage.getItem("auth-token")) {
-      fetch(`http://localhost:4000/getcart`, {
+      fetch(`https://wish-backend.onrender.com/getcart`, {
         method: "POST",
         headers: {
           Accept: "application/json",
@@ -38,7 +38,7 @@ const ShopContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: (prev[itemId] || 0) + 1 }));
 
     if (localStorage.getItem("auth-token")) {
-      fetch(`http://localhost:4000/addtocart`, {
+      fetch(`https://wish-backend.onrender.com/addtocart`, {
         method: "POST",
         headers: {
           Accept: "application/form-data",
@@ -62,7 +62,7 @@ const ShopContextProvider = (props) => {
     });
 
     if (localStorage.getItem("auth-token")) {
-      fetch(`http://localhost:4000/removefromcart`, {
+      fetch(`https://wish-backend.onrender.com/removefromcart`, {
         method: "POST",
         headers: {
           Accept: "application/form-data",
