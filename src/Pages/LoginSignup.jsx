f05e8af534bbd59561cdcbe739a10bbc1f3563c8
your_name_here <cssprasanth99@gmail.com>
2024-08-01 15:54:48 +0530

    updated loginsignup

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -21,7 +21,7 @@ const LoginSignup = () => {
   const login = async () => {
     console.log("Login", formData);
     let responseData;
-    await fetch(`http://localhost:4000/login`, {
+    await fetch(`https://wish-backend.onrender.com/login`, {
       method: "POST",
       headers: {
         Accept: "application/form-data",
@@ -44,7 +44,7 @@ const LoginSignup = () => {
   const signup = async () => {
     console.log("Register", formData);
     let responseData;
-    await fetch(`http://localhost:4000/signup`, {
+    await fetch(`https://wish-backend.onrender.com/signup`, {
       method: "POST",
       headers: {
         Accept: "application/form-data",
